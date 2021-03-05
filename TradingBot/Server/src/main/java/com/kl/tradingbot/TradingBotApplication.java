a08580b8436000cd9d7aa8e1c1eaaad360982196
Kostadin Lambov <klambov20@gmail.com>
2021-03-05 15:14:54 +0100

    Server: Added initial configuration

---
+++ TradingBot/Server/src/main/java/com/kl/tradingbot/TradingBotApplication.java
@@ -0,0 +1,13 @@
+package com.kl.tradingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingBotApplication.class, args);
+    }
+
+}
---
+++ TradingBot/Server/src/test/java/com/kl/tradingbot/KlTraidingbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kl.tradingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlTraidingbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
