26e05979c58a0291b868bef29db88707d74be180
Kostadin Lambov <klambov20@gmail.com>
2021-03-22 00:22:23 +0100

    Added footer, styled components config

---
+++ TradingBot/client/src/components/styledComponents/theme.js
@@ -0,0 +1,116 @@
+export const light = {
+  backgroundColor: '#E8EAED',
+  color: '#4E5B6E',
+  cards: {
+    borderColor: '#E5E9F2',
+    backgroundColor: '#FFF',
+    color: '#000',
+    title: {
+      color: '#4BD5EE',
+    },
+    subtitle: {
+      color: '#ABB1BA',
+    },
+  },
+  solidButton: {
+    backgroundColor: '#000',
+    borderColor: '#E5E9F2',
+    color: '#FFE300',
+  },
+  outlinedButton: {
+    backgroundColor: '#EFF2F7',
+    borderColor: '#E5E9F2',
+    color: 'inherit',
+  },
+  input: {
+    backgroundColor: '#EFF2F7',
+    borderColor: '#E5E9F2',
+    color: '#3C4858',
+  },
+  appBar: {
+    backgroundColor: '#000',
+    borderColor: '#333',
+    color: '#4BD5EE',
+  },
+  h1: {
+    color: '#4BD5EE',
+  },
+  h2: {
+    color: '#4BD5EE',
+  },
+  h3: {
+    color: '#4BD5EE',
+  },
+  h4: {
+    color: '#4BD5EE',
+  },
+  h5: {
+    color: '#4BD5EE',
+  },
+  h6: {
+    color: '#4BD5EE',
+  },
+  spinnerColor: {
+    color: '#000',
+  },
+};
+
+export const dark = {
+  backgroundColor: '#000',
+  color: '#ABB1BA',
+  cards: {
+    borderColor: '#3C4858',
+    backgroundColor: '#333',
+    color: '#FFF',
+    title: {
+      color: '#FFE300',
+    },
+    subtitle: {
+      color: '#4BD5EE',
+    },
+  },
+  solidButton: {
+    backgroundColor: '#4BD5EE',
+    borderColor: 'none',
+    color: '#FFE300',
+  },
+
+  outlinedButton: {
+    backgroundColor: '#333',
+    borderColor: '#3C4858',
+    color: 'inherit',
+  },
+
+  input: {
+    backgroundColor: '#FFF',
+    borderColor: '#3C4858',
+    color: '#273444',
+  },
+  appBar: {
+    backgroundColor: '#333',
+    borderColor: '#3C4858',
+    color: '#4BD5EE',
+  },
+
+  h1: {
+    color: '#FFE300',
+  },
+  h2: {
+    color: '#FFE300',
+  },
+  h3: {
+    color: '#FFE300',
+  },
+  h4: {
+    color: '#FFE300',
+  },
+  h5: {
+    color: '#FFE300',
+  },
+  h6: {
+    color: '#FFE300',
+  },
+  spinnerColor: {
+    color: '#4BD5EE',
+  },
+};
